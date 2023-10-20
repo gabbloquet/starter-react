7a41a6baf2135494e6cfedf07e0a6fad95faaef2
Gabin BLOQUET <gabin.blqt@gmail.com>
2023-10-20 10:52:22 +0200

    fix: migration to storybook 7

--- src/containers/Menu/Menu.stories.tsx
+++ src/containers/Menu/Menu.stories.tsx
@@ -1,14 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
-import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import { Menu } from './index';
 
 export default {
   title: 'Menu',
   component: Menu
-} as ComponentMeta<typeof Menu>;
+};
 
-const Template: ComponentStory<typeof Menu> = () => (
+const Template = () => (
   <BrowserRouter>
     <Menu />;
   </BrowserRouter>
